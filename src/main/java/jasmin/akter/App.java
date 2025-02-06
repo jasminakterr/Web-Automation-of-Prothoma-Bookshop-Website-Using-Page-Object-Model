8424ce6d3b47fe152a3ea1911ad1600c4b19aa2f
Jasmin Akter Purnima <jasminakterpurnima47@gmail.com>
2025-02-06 12:36:33 +0600

    First Commit

---
+++ src/main/java/jasmin/akter/App.java
@@ -0,0 +1,13 @@
+package jasmin.akter;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
